44b486c0dc0cf00d8c4177264c302f4e4efa2bac
ravi <ravi@gmail.com>
2025-04-23 04:37:30 +0000

    updated-by-ravi

--- demo.java
+++ demo.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.out.println("Welcome to Java programming.");
+        System.out.println("Have a great day!");
     }
 }
 
